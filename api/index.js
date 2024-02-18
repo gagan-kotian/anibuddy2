f70fb27f724c7fa0db98de12a25095a5d882fc7e
gagan-kotian <gagankotian48@gmail.com>
2024-02-18 18:53:04 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://gagankotian48:wIv4Az8FF1d9TbBZ@anibuddycluster.oywic6e.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to mongoose");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server connected");
